6d256f2093ace18066cf388df2bb3c1e20f36db7
darya_kolyadko <skvoznak1996@gmail.com>
2016-12-12 00:32:19 +0300

    try fix DI #2

--- src/main/java/com/kolyadko_polovtseva/book_maze/dao/UserRepository.java
+++ src/main/java/com/kolyadko_polovtseva/book_maze/dao/UserRepository.java
@@ -2,12 +2,13 @@ package com.kolyadko_polovtseva.book_maze.dao;
 
 import com.kolyadko_polovtseva.book_maze.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by DaryaKolyadko on 09.12.2016.
  */
-@Repository("userRepository")
+@Component("userRepository")
 public interface UserRepository extends CrudRepository<User, String> {
     User findUserByLoginAndPassword(String login, String password);
 }
